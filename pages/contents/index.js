c7136c6c02be70e5a33f7826a5583ec7a372c429
yu <77177966+andyu17@users.noreply.github.com>
2023-04-13 05:46:12 +0000

    map導入中（ブルー画面状態）

--- pages/contents/index.js
+++ pages/contents/index.js
@@ -2,18 +2,7 @@
 export default function ProductsList() {
     return (
         <div> 
-                <h2 >商品一覧</h2>
-                <ul>
-                    <li>
-                            スマートフォン
-                    </li>
-                    <li>
-                            パソコン
-                    </li>
-                    <li>
-                            ヘッドフォン
-                    </li>
-                </ul>
+                <h2 >DASHBOARD!!!!!!!!!!!!!</h2>
         </div>
     );
 }
